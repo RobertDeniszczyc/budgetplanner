9f444f892d0df5b7d58773822e1a0d66cfa60aa4
Rob Deniszczyc <robdeniszczyc@gmail.com>
2020-01-21 19:49:31 +0000

    Added docblocks into calculator method

--- src/components/calculators/OutgoingCalculator.js
+++ src/components/calculators/OutgoingCalculator.js
@@ -1,6 +1,18 @@
+/**
+ * Calcuate the total outgoings
+ * 
+ * @param {object} state The full state of all outgoings in the application
+ * @param {object} inTransition Which outgoing amount is triggering this
+ * @param {number} payload What is the amount being changed to
+ */
 export default function OutgoingCalculator(state, inTransition, payload) {
   let total = 0;
 
+  /** 
+   * Delete the amount from the state which is being changed
+   * We do this so we can easily add everything else together
+   * then add on the changing amount at the end.
+   */
   if (inTransition) {
     delete state[inTransition];
   }
@@ -13,5 +25,11 @@ export default function OutgoingCalculator(state, inTransition, payload) {
     total += payload;
   }
 
+  /**
+   * Return the total
+   * Note: This method ONLY returns a total, so it doesn't matter
+   *       that we were deleting items from "state" above. We haven't 
+   *       actually touched the real application state.
+   */
   return total;
 }
